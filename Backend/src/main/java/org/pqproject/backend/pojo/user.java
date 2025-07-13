b2567c28071a8a768ff189f24d68dc732ee2bb86
botoya <437119214@qq.com>
2025-07-13 22:18:26 +0800

    更改

---
+++ Backend/src/main/java/org/pqproject/backend/pojo/user.java
@@ -0,0 +1,58 @@
+package org.pqproject.backend.pojo;
+
+public class user {
+    private Long userId;
+    private String email;
+    private String password;
+    private String username;
+    private String userPhone;
+    private String role;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
